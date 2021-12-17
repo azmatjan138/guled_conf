a7a2f72935779eb70b78e541549657676915e1f9
azmatjan138 <70242939+azmatjan138@users.noreply.github.com>
2021-12-17 20:42:20 +0800

    Add files via upload

--- logstash.sh
+++ logstash.sh
@@ -15,15 +15,15 @@ else
 fi
 
 dir=`pwd`
-curl -O https://artifacts.elastic.co/downloads/logstash/logstash-7.13.0-linux-x86_64.tar.gz --output arthas
+curl -O https://artifacts.elastic.co/downloads/logstash/logstash-7.2.0.tar.gz --output arthas
 #wget https://artifacts.elastic.co/downloads/logstash/logstash-7.2.0-linux-x86_64.tar.gz
-tar -zxvf ${dir}/logstash-7.13.0-linux-x86_64.tar.gz
+tar -zxvf ${dir}/logstash-7.2.0.tar.gz
 
 export JAVA_HOME=${dir}/logstash-7.13.0/jdk
 export JRE_HOME=${JAVA_HOME}/jre
 export CLASSPATH=.:${JAVA_HOME}/lib:${JRE_HOME}/lib
 export PATH=${JAVA_HOME}/bin:$PAT
 git clone "https://github.com/azmatjan138/guled_conf.git"
-mv guled_conf ${dir}/logstash-7.13.0
+mv guled_conf ${dir}/logstash-7.2.0
 echo "Install complete"
-echo "Please edit your config file , And run : ./logstash-7.13.0/bin/logstash -f ./logstash-7.13.0/guled_conf/conf/mysql.conf"
+echo "Please edit your config file , And run : ./logstash-7.13.0/bin/logstash -f ./logstash-7.2.0/guled_conf/conf/mysql.conf"
